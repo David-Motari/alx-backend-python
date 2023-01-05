4267519bbabf65a62168aa25bad6033e7774e43e
David_Motari <davemotari22@gmail.com>
2023-01-05 20:29:16 +0300

    Task 10

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+"""
+100-safe_first_element
+"""
+from typing import Sequence, Any, Union
+
+
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, None]:
+    """
+    The types of the elements of the input are not know
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
